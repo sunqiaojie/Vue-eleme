1f309443ee159991a944b0e20f46863bbe01d141
sunqiaojie <437192731@qq.com>
2017-03-22 18:28:29 +0800

    update seller rating

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -4,7 +4,7 @@
 			<ul>
 				<li v-for="(item, index) in goods" class="menu-item" :class="{'current':currentIndex===index}" @click="selectMenu(index, $event)">
 					<span class="text border-1px">
-						<span v-show="item.type" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+						<span v-show="item.type>=0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
 					</span>
 				</li>
 			</ul>
@@ -14,7 +14,7 @@
 				<li v-for="item in goods" class="food-list food-list-hook">
 					<h1 class="title">{{item.name}}</h1>
 					<ul>
-						<li v-for="food in item.foods" class="food-item border-1px">
+						<li @click="selectFood(food,$event)" v-for="food in item.foods" class="food-item border-1px">
 							<div class="icon">
 								<img width="57" height="57" :src="food.icon">
 							</div>
@@ -38,12 +38,15 @@
 			</ul>
 		</div>
 		<shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice" ></shopcart>
+		<food :food="selectedFood" ref='food'></food>
 	</div>
 </template>
 <script type="text/ecmascript-6">
 	import BScroll from 'better-scroll';
 	import shopcart from '../shopcart/shopcart.vue';
 	import cartcontrol from '../cartcontrol/cartcontrol.vue';
+	import food from '../food/food.vue';
+
 	const ERR_OK = 0;
 
 	export default {
@@ -56,7 +59,8 @@
 			return {
 				goods: [],
 				listHeight: [],
-				scrollY: 0
+				scrollY: 0,
+				selectedFood: {}
 			};
 		},
 		computed: {
@@ -105,6 +109,13 @@
 				let el = foodList[index];
 				this.foodsScroll.scrollToElement(el, 300);
 			},
+			selectFood(food, event) {
+				if (!event._constructed) {
+					return;
+				}
+				this.selectedFood = food;
+				this.$refs.food.show();
+			},
 			_initScroll() {
 				this.menuScroll = new BScroll(this.$refs.menuWrapper, {
 					click: true
@@ -131,7 +142,8 @@
 		},
 		components: {
 			shopcart,
-			cartcontrol
+			cartcontrol,
+			food
 		}
 	};
 </script>
